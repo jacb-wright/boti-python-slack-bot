214dd74b2a7064c5438f0bc169c4fc8dd803e1c3
jacb-wright <79611506+jacb-wright@users.noreply.github.com>
2021-02-25 21:00:58 -0600

    Update boti-test.py

--- boti-test.py
+++ boti-test.py
@@ -24,14 +24,15 @@ from slackeventsapi import SlackEventAdapter
 import json
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import sys
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['31b87437c2a6aff6d7c7a119d6c82c78'], '/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['xoxb-4004473389-1779407041846-FjKM0OP48cxqf9HNFULPu5Mf'])
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
 # Integrate with Ravi's code to define list of calendar events for current day
@@ -40,48 +41,78 @@ count_events = len(list_events)
 
 # CHANGE THIS TO THE CHANNEL YOU ARE TESTING IN
 channel_test = '#bot-test-jacob'
+channel_test_id = 'C01NVQA3FFX'
 
+# intro message asking if user wants to track time
 client.chat_postMessage(
     channel=channel_test,
-    text="It's time to track your time! Reply 'Fine.' to continue or 'Go away!' to cancel"
+    blocks= [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Boti here!* :robot_face: Let's track your time! :timer_clock: \n\nReply with *'Track'* to coninute or *'Bye'* to cancel..."
+			}
+		}
+	]
 )
 
+# accepts user's response continuously. if statements will control bot's reaction to user
 @slack_event_adapter.on('message')
 def message(payload):
-    # uncomment the below to view the JSON payload retrieved
+    # uncomment below to see out from user (can be used to get user_id, channel_id, etc.)
     # print(payload)
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
 
-    # locate channel_id using print(payload) in line 52. copy and paste to channel_id == ''
-    if ((BOT_ID != user_id) and (channel_id == 'C01NVQA3FFX') and (text == 'Fine.')):
-        track_time = 'Yes'
-
-        while track_time == 'Yes':
+    # filters response to only use non-bot and only in test channel
+    if ((BOT_ID != user_id) and (channel_id == channel_test_id)):
+        
+        # user responds they do want to track time
+        if (text.lower() == 'track'):
             client.chat_postMessage(
                 channel=channel_test,
-                text='This is where the code will go to ask about each list item and track the user's input. Blank until I get the important stuff working.'
+                text="yes, track"
             )
-            track_time = 'No'
+
+        # user responds they dont want to track time
+        if (text.lower() == 'bye'):
+            client.chat_postMessage(
+                channel=channel_test,
+                text='Rude. Thanks anyway. Have a great day. Boti out!'
+            )
         
-    # locate channel_id using print(payload) in line 52. copy and paste to channel_id == ''
-    if ((BOT_ID != user_id) and (channel_id == 'C01NVQA3FFX') and (text == 'Go away!')):
-        client.chat_postMessage(
-        channel=channel_test,
-        text='Geez, okay. Thanks anyway. Have a great day. Boti out!'
-        )
-        quit()
+        if (text.lower() == 'kudos'):
+            client.chat_postMessage(
+                channel=channel_test,
+                blocks= [
+		            {
+			            "type": "section",
+			            "text": {
+			            	"type": "mrkdwn",
+			            	"text": "<!channel> *Jacob* tracked his time for today! :clap:"
+			            }
+		            }
+	            ]
+            )
+               
+        # error in response if no active input detected
+        else:
+            client.chat_postMessage(
+                channel=channel_test,
+                blocks= [
+		            {
+			            "type": "section",
+			            "text": {
+			            	"type": "mrkdwn",
+			            	"text": ":warning: *Error: You confused Boti* :white_frowning_face: \n\nBoti does not yet support typos... unless you fund this project :wink:"
+			            }
+		            }
+	            ]
+            )
 
-    # locate channel_id using print(payload) in line 52. copy and paste to channel_id == ''
-    if ((BOT_ID != user_id) and (channel_id == 'C01NVQA3FFX') and (text != 'Go away!' or 'Fine.')):
-        client.chat_postMessage(
-        channel=channel_test,
-        text='Error. You confused Boti. Boti does not yet support typos... unless you fund this project ;)...'
-        )
-        quit()
-
-
+# debugger which coincidently causes messages to send twice, will not work without it though
 if __name__ == "__main__":
     	app.run(debug=True)
